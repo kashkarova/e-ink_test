806644a5a020c5005fee3f68613ee9370b0d3ab2
Tetiana Kashkarova <tetiana_kashkarova@epam.com>
2020-06-11 20:45:12 +0300

    Modified index.html

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import $ from 'jquery';
+
+function updateDateTime(): void {
+    const url = 'http://worldtimeapi.org/api/ip';
+    $.get(url, function (data) {
+        $('#current-date-time').text(data['datetime']);
+    });
+}
+
+$(document).ready(function () {
+    setInterval(function () {
+        updateDateTime();
+     }, 60000);
+});
