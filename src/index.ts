6cd091ed919bd81ff91b9206ef20e78613a79561
Tetiana Kashkarova <tetiana_kashkarova@epam.com>
2020-06-16 23:26:13 +0300

    Modified request string

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 
 function updateDateTime(): void {
-    const url = 'http://worldtimeapi.org/api/ip';
+    const url = 'https://worldtimeapi.org/api/ip';
     $.get(url, function (data) {
         $('#current-date-time').text(data['datetime']);
     });
